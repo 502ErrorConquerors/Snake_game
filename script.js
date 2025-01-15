134570a3cad8b5e9f9e2887560c86be0942a05f9
dev pathak <devpathak@devs-MacBook-Air.local>
2025-01-16 01:13:44 +0530

    sab kar diya

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Music Files
 const foodSound = new Audio('music/food.mp3');
-const gameOverSound = new Audio('music/gameover.mp3');
+const gameOverSound = new Audio('music/gameOver.mp3');
 const moveSound = new Audio('music/move.mp3');
 const musicSound = new Audio('music/music.mp3');
 
