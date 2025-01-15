669570295c993ad226874393830cc57dfea1c16a
Ansh Saxena <anshsaxena6205@gmail.com>
2025-01-15 17:27:39 +0530

    added loading

---
+++ script.js
@@ -0,0 +1,9 @@
+//Music Files
+const foodSound = new Audio('music/food.mp3');
+const gameOverSound = new Audio('music/gameover.mp3');
+const moveSound = new Audio('music/move.mp3');
+const musicSound = new Audio('music/music.mp3');
+
+//DOM Loading
+const canvas = document.getElementById('screen');
+const ctx = canvas.getContext('2d');
